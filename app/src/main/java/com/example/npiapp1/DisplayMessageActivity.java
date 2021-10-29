4f16f5054f384b7e2ead5c03d49bbfd548e8ae58
Sad-Potato <e.sergiofern@go.ugr.es>
2021-10-29 11:57:12 +0200

    NFC testing in progress

--- app/src/main/java/com/example/npiapp1/DisplayMessageActivity.java
+++ app/src/main/java/com/example/npiapp1/DisplayMessageActivity.java
@@ -3,7 +3,10 @@ package com.example.npiapp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -19,7 +22,34 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
 
+        //Context context;
+        //NfcManager manager = (NfcManager) context.getSystemService(Context.NFC_SERVICE);
+        //NfcAdapter adapter = manager.getDefaultAdapter();
+
+
+        //textView.setText("TEST 1212");
+
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        String test="";
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Parcelable[] rawMessages =
+                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMessages != null) {
+                NdefMessage[] messages = new NdefMessage[rawMessages.length];
+                for (int i = 0; i < rawMessages.length; i++) {
+                    messages[i] = (NdefMessage) rawMessages[i];
+                }
+                test=messages[0].toString();
+                // Process the messages array.
+            }
+        }
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(test);
     }
 }
