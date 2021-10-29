1f7a7560a806ae3cc6035d72ae8e12651bc96c7b
Miguel Moles <molesmiguel@gmail.com>
2021-10-29 11:52:25 +0200

    Metidas cosas de prueba de sensores y otras cosas graciosas

--- app/src/main/java/com/example/npiapp1/MainActivity.java
+++ app/src/main/java/com/example/npiapp1/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.npiapp1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private SensorManager sensorManager;
+    private List<Sensor> deviceSensors;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
     }
 
     public void sendMessage(View view){
@@ -20,5 +33,27 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void startTest(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        TextView textView = findViewById(R.id.textView2);
+        String t = "";
+        for (Sensor s : deviceSensors) {
+            t += s.getName() + ", " + String.valueOf(s.getMinDelay()) + "\n";
+        }
+        textView.setText(t);
+
+        /*new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                textView.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                textView.setText("done!");
+            }
+        }.start();
+        */
     }
 }
