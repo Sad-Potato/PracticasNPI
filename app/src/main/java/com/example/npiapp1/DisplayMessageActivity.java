0e5164c8395cd020a2c604b39ee1a992070a7d08
Sad-Potato <e.sergiofern@go.ugr.es>
2021-11-02 18:11:30 +0100

    Two ways of getting QR, with native android libraries or with ZXING

--- app/src/main/java/com/example/npiapp1/DisplayMessageActivity.java
+++ app/src/main/java/com/example/npiapp1/DisplayMessageActivity.java
@@ -1,25 +1,42 @@
 package com.example.npiapp1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.media.Image;
-import android.nfc.NdefMessage;
-import android.nfc.NfcAdapter;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.provider.MediaStore;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.mlkit.vision.barcode.Barcode;
 import com.google.mlkit.vision.barcode.BarcodeScanner;
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
 import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.common.InputImage;
+
+import java.util.List;
+
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    InputImage image;
+
+    BarcodeScannerOptions options =
+            new BarcodeScannerOptions.Builder()
+                    .setBarcodeFormats(
+                            Barcode.FORMAT_QR_CODE)
+                    .build();
+
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -27,6 +44,69 @@ public class DisplayMessageActivity extends AppCompatActivity {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         } catch (ActivityNotFoundException e) {
             System.out.println("Error");
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Despues de echar la foto obtenemos la imagen en forma de bitmap
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            // Con esto lo único que hacemos es mostrar la imagen (thumnail) en el
+            // widget
+            ImageView imageView=findViewById(R.id.imageTest);
+            imageView.setImageBitmap(imageBitmap);
+            // Pasamos el bitmap a un formato InputImage para
+            // usarla con barcode scanning
+            int rotationDegree = 0;
+            image=InputImage.fromBitmap(imageBitmap, rotationDegree);
+            // Escaneamos con los parametros que hemos definido
+            BarcodeScanner scanner = BarcodeScanning.getClient(options);
+
+            // Procesamos el qr
+            Task<List<Barcode>> result = scanner.process(image)
+                    .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                        @Override
+                        public void onSuccess(List<Barcode> barcodes) {
+                            // Task completed successfully
+                            // [START_EXCLUDE]
+                            // [START get_barcodes]
+                            for (Barcode barcode: barcodes) {
+                                Rect bounds = barcode.getBoundingBox();
+                                Point[] corners = barcode.getCornerPoints();
+
+                                String rawValue = barcode.getRawValue();
+
+                                int valueType = barcode.getValueType();
+                                // See API reference for complete list of supported types
+                                switch (valueType) {
+                                    case Barcode.TYPE_WIFI:
+                                        String ssid = barcode.getWifi().getSsid();
+                                        String password = barcode.getWifi().getPassword();
+                                        int type = barcode.getWifi().getEncryptionType();
+                                        break;
+                                    case Barcode.TYPE_URL:
+                                        String title = barcode.getUrl().getTitle();
+                                        String url = barcode.getUrl().getUrl();
+                                        TextView textView = findViewById(R.id.textView);
+                                        textView.setText(url);
+                                        break;
+                                }
+                            }
+                            // [END get_barcodes]
+                            // [END_EXCLUDE]
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Task failed with an exception
+                            // ...
+                        }
+                    });
         }
     }
 
@@ -43,9 +123,8 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
 
+
         dispatchTakePictureIntent();
-
-
     }
 
 }
--- app/src/main/java/com/example/npiapp1/MainActivity.java
+++ app/src/main/java/com/example/npiapp1/MainActivity.java
@@ -67,23 +67,8 @@ public class MainActivity extends AppCompatActivity {
 
         IntentIntegrator intentIntegrator = new IntentIntegrator(this);
         intentIntegrator.setPrompt("Scan a barcode or QR Code");
-        intentIntegrator.setOrientationLocked(true);
+        //intentIntegrator.setOrientationLocked(false);
         intentIntegrator.initiateScan();
-
-        try {
-
-            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-            intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
-
-            startActivityForResult(intent, 0);
-
-        } catch (Exception e) {
-
-            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
-            Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
-            startActivity(marketIntent);
-
-        }
 
         /*
         for (Sensor s : deviceSensors) {
