4681bf459cbd30dab637f86ad942f01fff9927d5
angel <angelaranda@correo.ugr.es>
2021-11-04 21:39:43 +0100

    El mapa funciona a medias y el zoom directamente no lo hace

--- app/src/main/java/com/example/npiapp1/DisplayMapActivity.java
+++ app/src/main/java/com/example/npiapp1/DisplayMapActivity.java
@@ -1,46 +1,86 @@
 package com.example.npiapp1;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayMapActivity extends AppCompatActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private ViewGroup mainLayout;
+    private ImageView image;
 
+    private int xDelta;
+    private int yDelta;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_map);
+
+        mainLayout = (RelativeLayout) findViewById(R.id.main);
+        image = (ImageView) findViewById(R.id.image);
+
+        image.setOnTouchListener(onTouchListener());
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        int currentX = 0;
-        int currentY = 0;
-        switch (event.getAction()) {
+    private View.OnTouchListener onTouchListener() {
+        return new View.OnTouchListener() {
 
-            case MotionEvent.ACTION_DOWN:
-            {
-                currentX = (int) event.getRawX();
-                currentY = (int) event.getRawY();
-                break;
-            }
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
 
-            case MotionEvent.ACTION_MOVE: {
-                int x2 = (int) event.getRawX();
-                int y2 = (int) event.getRawY();
-                View container = null;
-                container.scrollBy(currentX - x2 , currentY - y2);
-                currentX = x2;
-                currentY = y2;
-                break;
+                final int x = (int) event.getRawX();
+                final int y = (int) event.getRawY();
+
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
+                                view.getLayoutParams();
+
+                        xDelta = x - lParams.leftMargin;
+                        yDelta = y - lParams.topMargin;
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+                                .getLayoutParams();
+                        layoutParams.leftMargin = x - xDelta;
+                        layoutParams.topMargin = y - yDelta;
+                        layoutParams.rightMargin = 0;
+                        layoutParams.bottomMargin = 0;
+                        view.setLayoutParams(layoutParams);
+                        break;
+                }
+
+                mainLayout.invalidate();
+                return true;
             }
-            case MotionEvent.ACTION_UP: {
-                break;
-            }
-        }
-        return true;
+        };
+    }
+
+    private void zoom(View view) {
+        ImageView img = (ImageView) findViewById(R.id.image);
+        Context applicationContext=getApplicationContext();
+        Animation animation = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom);
+        img.startAnimation(animation);
+    }
+
+    private void zoom_out(View view) {
+        ImageView img = (ImageView) findViewById(R.id.image);
+        Context applicationContext=getApplicationContext();
+        Animation animation = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom_out);
+        img.startAnimation(animation);
     }
 }
